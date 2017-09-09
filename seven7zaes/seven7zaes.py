a991dcd16b3e399c8fe2acbcfda3935f064ef32c
jfishe <jdfenw@gmail.com>
2017-09-09 11:27:45 -0700

    Migrate from Jupyter Notebook
    Copy working parts of Notebook into module.
    
    TODO: Refactor into CLI and move statements into functions.

--- seven7zaes/seven7zaes.py
+++ seven7zaes/seven7zaes.py
@@ -1,3 +1,46 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from pathlib import Path
+import subprocess
+
+
+p = Path(os.getenv('USERPROFILE') + r'\Pictures\N')
+method = Path(os.getenv('ProgramFiles') + r'\7-zip')
+
+cwd = Path(os.getenv('TEMP') + r'\work')
+
+try:
+    os.mkdir(cwd)
+except FileExistsError as err:
+    print(err)
+
+parent = (dn for dn in p.glob('*') if dn.is_file() is False)
+
+def get_dirs(path):
+    dn = ''
+    for dn in path.glob('*'):
+        if dn.name != dn.parent.name:
+            return (dn.parent.name, dn.parent)
+        else:
+            return get_dirs(dn)
+    return None
+
+dirs = (get_dirs(dn) for dn in parent)
+
+def compress(name, src, cwd, method, password):
+
+    cmd = ['7z', 'a', name,
+       r'-r', '-p' + password, '-mhe', src + r'\*']
+
+    env = {"PATH": r'C:\Program Files\7-Zip'}
+
+    return subprocess.run(cmd, cwd=cwd, shell=True, env=env,
+               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+for name, src in dirs:
+    result = compress(name, str(src.absolute()), cwd, method)
+#     print(result.args)
+    print(result.stderr.decode() or None)
+
